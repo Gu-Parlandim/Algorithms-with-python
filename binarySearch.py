b6ccb2e5be605b2357a8ebc40b7812024dfafd7d
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-09-21 15:35:37 -0300

    pesquisa binaria

---
+++ binarySearch.py
@@ -0,0 +1,19 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1  # array length
+    while low <= high:
+        middle = int((low + high) / 2)
+        shot = list[middle]
+
+        if (shot == item):
+            return middle  # return item indice
+        if (shot > item):
+            high = middle - 1  # update max value
+        else:
+            low = middle + 1
+    return None
+
+
+my_array = [5, 10, 15, 20, 25, 41, 43, 44, 50, 60, 80]
+
+print(binary_search(my_array, 80))
